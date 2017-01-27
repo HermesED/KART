c2a7c59b351237e3db6062f741b5b9c18cbb1b49
HermesED <hermesed23@gmail.com>
2017-01-27 19:51:30 +0700

    Update Kegiatan_model.php

--- KART/application/models/Kegiatan_model.php
+++ KART/application/models/Kegiatan_model.php
@@ -16,26 +16,41 @@ class Kegiatan_model extends CI_Model {
 
 
     public function get_list_kegiatan(){
-        $sql = "SELECT n.judul, n.id id, n.kegiatan, n.tanggal, n.penulis
+        $sql = "SELECT n.judul, n.id id_keg, n.kegiatan, n.tanggal, n.penulis,
+                       m.nim id_penulis
                 FROM kegiatan n
                 LEFT JOIN anggota_ukm m
-                    ON(n.penulis=nim)
+                    ON(n.penulis=m.nim)
                 ORDER BY n.tanggal DESC
-                LIMIT 10";
+                LIMIT 3";
         $query = $this->db->query( $sql );
-       
+        if(( $query->result_array() )) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
     }
-    public function get_kegiatan( $id ){
-        $sql = "SELECT n.judul, n.id id, n.kegiatan, n.tanggal, n.penulis
+
+    public function get_kegiatan( $idkeg ){
+        $sql = "SELECT n.judul, n.id id_keg, n.kegiatan, n.tanggal, n.penulis,
+                       m.nim id_penulis, n.like, n.view
                 FROM kegiatan n
                 LEFT JOIN anggota_ukm m
-                    ON(n.penulis=nim)
-                WHERE n.id='$id'";
+                    ON(n.penulis=m.nim)
+                WHERE n.id='$idkeg'";
         $query = $this->db->query( $sql );
-        
+        if(( $query->row_array() )) {
+            $sqlv = "UPDATE kegiatan SET `view`=`view`+1
+                    WHERE id=$idkeg";
+            $queryv = $this->db->query($sqlv);
+            return $query->row_array();
+        } else {
+            return false;
+        }
     }
-    public function foreword( $berita ){
-        $max = 200;  // maks karakter
+
+     public function foreword( $berita ){
+        $max = 500;  // maks karakter
         $fore = "";  // potongan berita maks 200 karakter
         $kata = explode( " ", $berita );
         foreach ($kata as $k) {
@@ -43,5 +58,20 @@ class Kegiatan_model extends CI_Model {
         }
         $fore .= "...";
         return $fore;
+    }
+    public function tambahlike( $id ){
+        if(( $_SESSION['sudahlike'.$id] )){
+            $sql = "UPDATE kegiatan SET `like`=`like`+1
+                    WHERE id=$id";
+            $query = $this->db->query($sql);
+            $_SESSION['sudahlike'.$id]=1;
+        }
+        $sql = "SELECT `like` FROM kegiatan WHERE id=$id";
+        $query = $this->db->query($sql);
+        if(( $query->row_array() )) {
+            return $query->row_array()['like'];
+        } else {
+            return false;
+        }
     }
 }
