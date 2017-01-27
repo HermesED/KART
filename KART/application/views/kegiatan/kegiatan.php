9ed4543e0f1df412023fc760c3ec625106a1a574
HermesED <hermesed23@gmail.com>
2017-01-27 19:55:08 +0700

    Update kegiatan.php

--- KART/application/views/kegiatan/kegiatan.php
+++ KART/application/views/kegiatan/kegiatan.php
@@ -1,37 +1,18 @@
 <head>
     <title><?= $title;?></title>
-    <link href="assets/css/lightbox.css" rel="stylesheet"> 
-    <link href="assets/css/animate.min.css" rel="stylesheet">
+    <link href="<?= base_url();?>assets/css/lightbox.css" rel="stylesheet"> 
+    <link href="<?= base_url();?>assets/css/animate.min.css" rel="stylesheet">
 </head><!--/head-->
 
 <body>
-
-<div class="col-sm-8 col-md-8">
 <?php
-    foreach ( $kegiatan as $berita ) {
-        if( substr( $berita['tanggal'], 0, 10 ) == date('Y-m-d') ){
+    foreach ( $news as $berita ) {
+        if( substr( $berita['tanggal'], 0, 3 ) == date('Y-m-d') ){
             $panel = 'panel-danger';
         } else {
             $panel = 'panel-info';
         }
 ?>
-        <div class="panel <?=$panel?>">
-            <div class="panel-heading">
-                <h4><?= $berita['judul'] ?></h4>
-                <?= $berita['penulis'] ." - ". $berita['tanggal']  ?>
-            </div>
-            <div class="panel-body">
-                <?= $this->kegiatan_model->foreword($berita['isi'])?><br />
-                <a href="<?=base_url()."kegiatan/".$berita['id']?>">
-                    <button>Baca selengkapnya</button>
-                </a>
-            </div>
-        </div>
-<?php
-    }
-?>
-</div>
-
     <section id="blog" class="padding-top">
         <div class="container">
             <div class="row">
@@ -40,87 +21,32 @@
                          <div class="col-sm-12 col-md-12">
                             <div class="single-blog single-column">
                                 <div class="post-thumb">
-                                    <a href="kegiatandetail"><img src="assets/images/blog/7.jpg" class="img-responsive" alt=""></a>
+                                    <a href="kegiatandetail"><img src="<?= base_url();?>assets/images/blog/7.jpg" class="img-responsive" alt=""></a>
                                     <div class="post-overlay">
                                        <span class="uppercase"><a href="#">14 <br><small>Feb</small></a></span>
                                    </div>
                                 </div>
                                 <div class="post-content overflow">
-                                    <h2 class="post-title bold"><a href="kegiatandetail">something</a></h2>
-                                    <h3 class="post-author"><a href="#">Posted by micron News</a></h3>
-                                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber [...]</p>
-                                    <a href="#" class="read-more">View More</a>
-                                    <div class="post-bottom overflow">
-                                        <ul class="nav navbar-nav post-nav">
-                                            <li><a href="#"><i class="fa fa-tag"></i>Creative</a></li>
-                                            <li><a href="#"><i class="fa fa-heart"></i>32 Love</a></li>
-                                            <li><a href="#"><i class="fa fa-comments"></i>3 Comments</a></li>
-                                        </ul>
+                                    <div class="panel <?=$panel?>">
+                                        <div class="panel-heading">
+                                            <h2 class="post-title bold"><?= $berita['judul'] ?></h2>
+                                            <?= $berita['penulis'] ." - ". $berita['tanggal']  ?>
+                                        </div>
+                                        <div class="panel-body">
+                                            <?= $this->kegiatan_model->foreword($berita['kegiatan'])?><br />
+                                            <a href="<?=base_url()."kegiatan/".$berita['id_keg']?>" class="read-more">Baca selengkapnya</a>
+                                        </div>
+                                        <div class="post-bottom overflow">
+                                            <ul class="nav navbar-nav post-nav">
+                                                <li><a href="#"><i class="fa fa-tag"></i>Creative</a></li>
+                                                <li><a href="#"><i class="fa fa-heart"></i>32 Love</a></li>
+                                                <li><a href="#"><i class="fa fa-comments"></i>3 Comments</a></li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-12">
-                            <div class="single-blog single-column">
-                                <div class="post-thumb">
-                                    <a href="kegiatandetail"><img src="assets/images/blog/8.jpg" class="img-responsive" alt=""></a>
-                                    <div class="post-overlay">
-                                       <span class="uppercase"><a href="#">14 <br><small>Feb</small></a></span>
-                                   </div>
-                                </div>
-                                <div class="post-content overflow">
-                                    <h2 class="post-title bold"><a href="kegiatandetail">Advanced business cards design</a></h2>
-                                    <h3 class="post-author"><a href="#">Posted by micron News</a></h3>
-                                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber [...]</p>
-                                    <a href="#" class="read-more">View More</a>
-                                    <div class="post-bottom overflow">
-                                        <ul class="nav navbar-nav post-nav">
-                                            <li><a href="#"><i class="fa fa-tag"></i>Creative</a></li>
-                                            <li><a href="#"><i class="fa fa-heart"></i>32 Love</a></li>
-                                            <li><a href="#"><i class="fa fa-comments"></i>3 Comments</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-12">
-                            <div class="single-blog single-column">
-                                <div class="post-thumb">
-                                    <a href="kegiatandetail"><img src="assets/images/blog/9.jpg" class="img-responsive" alt=""></a>
-                                    <div class="post-overlay">
-                                       <span class="uppercase"><a href="#">14 <br><small>Feb</small></a></span>
-                                   </div>
-                                </div>
-                                <div class="post-content overflow">
-                                    <h2 class="post-title bold"><a href="kegiatandetail">Advanced business cards design</a></h2>
-                                    <h3 class="post-author"><a href="#">Posted by micron News</a></h3>
-                                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber [...]</p>
-                                    <a href="#" class="read-more">View More</a>
-                                    <div class="post-bottom overflow">
-                                        <ul class="nav navbar-nav post-nav">
-                                            <li><a href="#"><i class="fa fa-tag"></i>Creative</a></li>
-                                            <li><a href="#"><i class="fa fa-heart"></i>32 Love</a></li>
-                                            <li><a href="#"><i class="fa fa-comments"></i>3 Comments</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog-pagination">
-                        <ul class="pagination">
-                          <li><a href="#">left</a></li>
-                          <li><a href="#">1</a></li>
-                          <li><a href="#">2</a></li>
-                          <li class="active"><a href="#">3</a></li>
-                          <li><a href="#">4</a></li>
-                          <li><a href="#">5</a></li>
-                          <li><a href="#">6</a></li>
-                          <li><a href="#">7</a></li>
-                          <li><a href="#">8</a></li>
-                          <li><a href="#">9</a></li>
-                          <li><a href="#">right</a></li>
-                        </ul>
                     </div>
                  </div>
                 <div class="col-md-3 col-sm-5">
@@ -129,7 +55,7 @@
                             <h3>Comments</h3>
                             <div class="media">
                                 <div class="pull-left">
-                                    <a href="#"><img src="assets/images/portfolio/project1.jpg" alt=""></a>
+                                    <a href="#"><img src="<?= base_url();?>assets/images/portfolio/project1.jpg" alt=""></a>
                                 </div>
                                 <div class="media-body">
                                     <h4><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit,</a></h4>
@@ -138,7 +64,7 @@
                             </div>
                             <div class="media">
                                 <div class="pull-left">
-                                    <a href="#"><img src="assets/images/portfolio/project2.jpg" alt=""></a>
+                                    <a href="#"><img src="<?= base_url();?>assets/images/portfolio/project2.jpg" alt=""></a>
                                 </div>
                                 <div class="media-body">
                                     <h4><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit,</a></h4>
@@ -147,26 +73,13 @@
                             </div>
                             <div class="media">
                                 <div class="pull-left">
-                                    <a href="#"><img src="assets/images/portfolio/project3.jpg" alt=""></a>
+                                    <a href="#"><img src="<?= base_url();?>assets/images/portfolio/project3.jpg" alt=""></a>
                                 </div>
                                 <div class="media-body">
                                     <h4><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit,</a></h4>
                                     <p>posted on  07 March 2014</p>
                                 </div>
                             </div>
-                        </div>
-                        <div class="sidebar-item categories">
-                            <h3>Categories</h3>
-                            <ul class="nav navbar-stacked">
-                                <li><a href="#">Lorem ipsum<span class="pull-right">(1)</span></a></li>
-                                <li class="active"><a href="#">Dolor sit amet<span class="pull-right">(8)</span></a></li>
-                                <li><a href="#">Adipisicing elit<span class="pull-right">(4)</span></a></li>
-                                <li><a href="#">Sed do<span class="pull-right">(9)</span></a></li>
-                                <li><a href="#">Eiusmod<span class="pull-right">(3)</span></a></li>
-                                <li><a href="#">Mockup<span class="pull-right">(4)</span></a></li>
-                                <li><a href="#">Ut enim ad minim <span class="pull-right">(2)</span></a></li>
-                                <li><a href="#">Veniam, quis nostrud <span class="pull-right">(8)</span></a></li>
-                            </ul>
                         </div>
                         <div class="sidebar-item tag-cloud">
                             <h3>Tag Cloud</h3>
@@ -179,20 +92,12 @@
                                 <li><a href="#">Product</a></li>
                             </ul>
                         </div>
-                        <div class="sidebar-item popular">
-                            <h3>Latest Photos</h3>
-                            <ul class="gallery">
-                                <li><a href="#"><img src="assets/images/portfolio/popular1.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="assets/images/portfolio/popular2.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="assets/images/portfolio/popular3.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="assets/images/portfolio/popular4.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="assets/images/portfolio/popular5.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="assets/images/portfolio/popular1.jpg" alt=""></a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <!--/#blog-->
+    <?php
+    }
+?>
