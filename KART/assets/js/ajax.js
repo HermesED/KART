c82fa319f06a56b143e9d9978b358627bc43f968
nifitayapardana <nifitayapardana14@ymail.com>
2017-02-26 23:07:38 +0800

    Update ajax.js

--- KART/assets/js/ajax.js
+++ KART/assets/js/ajax.js
@@ -1,16 +1,162 @@
-$("#nimdftr").change( function(){
+$("#dftrnim").change( function(){
+    var noprodi=['510','520','310','320']
+    var d = new Date();
+    var tahun = d.getFullYear().toString();
+
+    var prodi = {
+        '510': "S1 - Sistem Informasi",
+        '520': "S1 - Teknik Informatika",
+        '310': "D3 - Sistem Informasi",
+        '320': "D3 - Teknik Informatika"
+    }
+    
+    var sel      = prodi[$(this).val().substr(0,3)];
+    var angkatan = $("#dftrnim").val().substr(3,2);
+
+    if(sel) {
+        $('#dftrprodi option[value="' + sel + '"]').attr("selected", "selected");
+        $("#dftrthn").val("20"+angkatan);
+    }
+    else{
+        ////
+    }
+
+    if (noprodi.indexOf($(this).val().substr(0,3)) < 0 || parseInt($(this).val().substr(3,2))+2000>tahun){
+        $("#dftrnim").attr("placeholder", "NIM Ini Tidak Valid!").val("").focus();
+        $("input[type='submit']").attr("disabled", true);
+        $("#dftrnama").attr("disabled", true);
+        $("#dftremail").attr("disabled", true);
+        $("#dftrthn").attr("disabled", true);
+        $("#software").attr("disabled", true);
+        $("#dftrhp").attr("disabled", true);
+    }
+    else
+    {
+        $.ajax({
+            url: "ajax/ceknim",
+            data: { "nim" : $(this).val() } ,
+            method: "POST",
+            success: function(result){
+                if( result == '1') {
+                    $("#dftrnim").attr("placeholder", "NIM Ini Sudah Terdaftar!").val("").focus();
+                    $("input[type='submit']").attr("disabled", true);
+                    $("#dftrnama").attr("disabled", true);
+                    $("#dftremail").attr("disabled", true);
+                    $("#dftrthn").attr("disabled", true);
+                    $("#software").attr("disabled", true);
+                    $("#dftrhp").attr("disabled", true);
+                } else {
+                    $("input[type='submit']").removeAttr("disabled");
+                    $("#dftrnama").removeAttr("disabled").focus();
+                    $("#dftremail").removeAttr("disabled");
+                    $("#dftrthn").removeAttr("disabled");
+                    $("#software").removeAttr("disabled");
+                    $("#dftrhp").removeAttr("disabled");
+                }
+            }
+        });
+    }
+} );
+
+$("#dftrnama").change( function(){
     $.ajax({
-        url: "ajax/ceknim",
-        data: { "nim" : $(this).val() } ,
+        url: "ajax/ceknama",
+        data: { "nama" : $(this).val() } ,
         method: "POST",
         success: function(result){
             if( result == '1') {
-                $("#warning_nim").html( "NIM sudah terdaftar" );
-                $(":submit").attr("disabled", true);
+                $("#dftrnama").attr("placeholder", "Nama Ini Sudah Ada!").val("").focus();
+                $("input[type='submit']").attr("disabled", true);
+                $("#dftremail").attr("disabled", true);
+                $("#dftrthn").attr("disabled", true);
+                $("#software").attr("disabled", true);
+                $("#dftrhp").attr("disabled", true);
             } else {
-                $("#warning_nim").html( "NIM OK" );
-                $(":submit").removeAttr("disabled");
+                $("input[type='submit']").removeAttr("disabled");
+                $("#dftremail").removeAttr("disabled").focus();
+                $("#dftrthn").removeAttr("disabled");
+                $("#software").removeAttr("disabled");
+                $("#dftrhp").removeAttr("disabled");
             }
         }
     });
 } );
+$("#dftremail").change( function(){
+    $.ajax({
+        url: "ajax/cekemail",
+        data: { "email" : $(this).val() } ,
+        method: "POST",
+        success: function(result){
+            if( result == '1') {
+                $("#dftremail").attr("placeholder", "Email Ini Sudah Terdaftar!").val("").focus();
+                $("input[type='submit']").attr("disabled", true);
+                $("#dftrthn").attr("disabled", true);
+                $("#software").attr("disabled", true);
+                $("#dftrhp").attr("disabled", true);
+            } else {
+                $("input[type='submit']").removeAttr("disabled");
+                $("#dftrthn").removeAttr("disabled");
+                $("#software").removeAttr("disabled");
+                $("#dftrhp").removeAttr("disabled");
+            }
+        }
+    });
+} );
+$("#nimbtl").change( function(){
+    $.ajax({
+        url: "ajax/ceknimbtl",
+        data: { "nim" : $(this).val() } ,
+        method: "POST",
+        success: function(result){
+            if( result == '1') {
+                $("#nimbtl").attr("placeholder", "NIM Ini Sudah Ada Dalam List!").val("").focus();
+                $("input[type='submit']").attr("disabled", true);
+                $("#namabtl").attr("disabled", true);
+                $("#angkatanbtl").attr("disabled", true);
+                $("#alasan").attr("disabled", true);
+                $("#pe_saran").attr("disabled", true);
+            } else {
+                $("input[type='submit']").removeAttr("disabled");
+                $("#namabtl").removeAttr("disabled").focus();
+                $("#angkatanbtl").removeAttr("disabled");
+                $("#alasan").removeAttr("disabled");
+                $("#pe_saran").removeAttr("disabled");
+            }
+        }
+    });
+} );
+
+$("#dftrhp").change( function(){
+    var nohp=['081','082','085','089','087']
+
+    if (nohp.indexOf($(this).val().substr(0,3)) < 0){
+        $("#dftrhp").attr("placeholder", "Bukan Nomor HP").val("").focus();}
+    else{
+        //
+    }
+} );
+
+//$("#dftrthn").change( function(){
+//    var d = new Date();
+//    var tahun = d.getFullYear().toString();
+
+//    $("#dftrthn").val(tahun);
+//} );
+
+$("#likenews").click( function(){
+    $.ajax({
+        url: "kegiatan/likenews",
+        success: function(result){
+            $("#jumlahlike").html( result );
+        }
+    });
+
+});
+//$(document).ready(function(){ 
+  //$("#form").find("input[type=textarea], input[type=password], textarea").each(function(ev){
+    //if(!$(this).val()) { 
+     //$(this).attr("placeholder", "Type your answer here");
+  //}
+  //});
+//});
+////////
